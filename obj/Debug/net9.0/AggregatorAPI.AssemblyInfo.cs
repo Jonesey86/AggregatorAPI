4889b7d8d89e2aec099d08a06f0c11c607585d46
Jonesey86 <irj082024@gmail.com>
2025-02-17 09:04:45 +0100

    initial commit

--- obj/Debug/net9.0/AggregatorAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/AggregatorAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AggregatorAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd835d201c505fa5e3c5ec6a94f44c2293c2259")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9735f4ede3a8c82dbd75b2ddbf4ecfbec2341cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AggregatorAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AggregatorAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
